3c25919bbf56edaeb774b54ae348a14a6569ccbe
Neha Abraham <me@nehabraham.com>
2019-01-23 14:10:35 -0500

    fix neighborhood land delete association

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,8 +1,8 @@
 class Neighborhood < ApplicationRecord
   validates :name, :city, :state, presence: true
   validates :state, length: { is: 2 }
-  has_many :lands
-  
+  has_many :lands, dependent: :destroy
+
   has_many_attached :images
 
   def city_state_name
--- test/integration/neighborhoods_lands_test.rb
+++ test/integration/neighborhoods_lands_test.rb
@@ -4,7 +4,9 @@ class NeighborhoodsLandsTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
   def setup
+    @admin = users(:admin_user)
     @neighborhood = neighborhoods(:lawrenceville)
+    @land = lands(:two)
   end
 
   test 'users can filter land by neighborhood' do
@@ -17,6 +19,16 @@ class NeighborhoodsLandsTest < ActionDispatch::IntegrationTest
     assert_template 'lands/index'
     assert_select 'h2', "#{@neighborhood.name} Land Lots"
     assert_select 'a[href=?]', land_path(@neighborhood.lands.first), text: 'Learn More'
+  end
+
+  test 'deleting neighborhood deletes lands' do
+    sign_in @admin
+    assert_difference('Land.count', -1) do
+      assert_difference('Neighborhood.count', -1) do
+        delete neighborhood_path(@neighborhood)
+      end
+    end
+
   end
 
 end
