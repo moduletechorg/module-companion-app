9f614206388d98f77ab4cd8376159e52f9654159
Neha Abraham <me@nehabraham.com>
2019-04-16 10:20:26 -0400

    remove b64 image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_155300) do
+ActiveRecord::Schema.define(version: 2019_04_11_185529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
