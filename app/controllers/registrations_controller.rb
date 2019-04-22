d0b2f6b444d95ab3c0e59030d724153c27e82eaf
Neha Abraham <me@nehabraham.com>
2019-04-22 10:45:23 -0400

    adjust routes after signup

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
     end
 
     def after_sign_up_path_for(resource)
+      flash[:success] = "Your account has been created! You will receive an email to confirm your account."
       lands_path
     end
 
